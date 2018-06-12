e956d550247dbc26fd0638880368d1cdb4e547b2
Tagir Ahkyamov <t.ahkyamov@elewise.com>
2018-06-12 20:00:44 +0400

    changed port

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var app = express();
 app.use("/", require("./web/app"));
 app.use("/api", require("./api/app"));
 
-app.listen(3000, function(){
-    console.log('Server listening on: 3000');
+app.listen(80, function(){
+    console.log('Server listening on: 80');
 });
